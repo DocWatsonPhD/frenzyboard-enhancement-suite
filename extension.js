73b549f2e8b2c0200b22121f6726be215cc51acc
DocWatsonPhD <docwatsonphd@gmail.com>
2016-02-13 23:59:32 -0700

    Create extension.js
    The meat of the extension. The DOM interaction will go in this file, with any unvailable operations (e.g. storage requests) being made through Chrome extension message passing (not yet implemented)

---
+++ extension.js
@@ -0,0 +1,9 @@
+// At this point I can interact with the DOM as normal,
+// but can't use variables or functions defined in the bootstrap script or the page itself.
+// Everything here (e.g. console statements) end up the visitor's main console
+
+console.log("extension.js is here!");
+
+//document.addEventListener('DOMContentLoaded', function(){
+//	console.log("extension.js DOMContentLoaded listener");
+//}
