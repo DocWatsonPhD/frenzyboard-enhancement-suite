b3b1a062edc9cea762d543c9ef9be4f9ba8da7b0
Stephen Watson <docwatsonphd@gmail.com>
2016-03-03 22:09:24 -0700

    Modify HTML for bubble span elements
    Adds span elements to the HTML of the page after loading. Unfortunately
    the test code won't be able to trigger with the old mouseovers on the
    TDs. Since I can't remove those listeners (since they're anonymous
    functions) I'm not sure what I can do about it.

--- extension.js
+++ extension.js
@@ -4,6 +4,64 @@
 
 console.log("extension.js is here!");
 
-//document.addEventListener('DOMContentLoaded', function(){
-//	console.log("extension.js DOMContentLoaded listener");
-//}
+//TODO: Figure out why the bootstrap code doesn't work for gating the extension...
+if ((window.location.href.toLowerCase().indexOf("frenzyboard.net") > -1) &&
+    ((window.location.href.toLowerCase().indexOf("?action=browse") > -1) ||
+	 (window.location.href.toLowerCase().indexOf("?action") === -1)))
+{
+	window.addEventListener('load', function()
+	{
+		console.log("extension.js onload listener");
+		getTopicClassRows();
+	});
+}
+
+function getTopicClassRows()
+{
+	var targetString = "window.status";
+	var returnTrueString = "; return true;";
+	// Get all TD tags with topic class
+	var rows = document.querySelectorAll("td.topic");
+	console.log(rows.length);
+	for (var i = 0; i < rows.length; i++) 
+	{
+		var outerHTML = rows[i].outerHTML.toLowerCase();
+		// Get status text
+		var statusPos = outerHTML.indexOf(targetString);
+		var statusText = '';
+		if (statusPos > -1)
+		{
+			var startPos = statusPos + targetString.length + 2;
+			var endPos = outerHTML.indexOf('"', startPos);
+			if (endPos == -1) 
+			{ 
+				continue;
+			}
+			statusText = outerHTML.substring(startPos, endPos - returnTrueString.length - 1);
+		}
+		if (statusText != '')
+		{
+			var spanElement = document.createElement('span');
+			// add visible=false attribute
+			var attr = document.createAttribute('visible');
+			attr.value = false;
+			spanElement.setAttributeNode(attr);
+			// add onmouseover attribute
+			attr = document.createAttribute('onmouseover');
+			attr.value = 'function() { log(\'' + statusText + '\'); }';
+			spanElement.setAttributeNode(attr);
+			spanElement.className = "bubble-item";
+			rows[i].appendChild(spanElement);
+		}
+	}
+}
+
+function log(str)
+{
+	console.log(str);
+}
+
+function extractPreviewText(node, indexOfStatusStr)
+{
+	
+}
