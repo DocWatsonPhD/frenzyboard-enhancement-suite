500859b2ab4cf2e28141890c30664bf4aaa56a68
Stephen Watson <docwatsonphd@gmail.com>
2016-03-16 00:49:37 -0700

    Add yt tag support
    Automatically embeds youtube videos given by URLs or IDs within matching
    [yt][/yt] tags

--- extension.js
+++ extension.js
@@ -5,17 +5,102 @@
 console.log("extension.js is here!");
 
 //TODO: Figure out why the bootstrap code doesn't work for gating the extension...
-if ((window.location.href.toLowerCase().indexOf("frenzyboard.net") > -1) &&
-    ((window.location.href.toLowerCase().indexOf("?action=browse") > -1) ||
-	 (window.location.href.toLowerCase().indexOf("?action") === -1)))
-{
-	window.addEventListener('load', function()
+if (window.location.href.toLowerCase().indexOf("frenzyboard.net") > -1)
+{	
+    if ((window.location.href.toLowerCase().indexOf("?action=browse") > -1) ||
+	    (window.location.href.toLowerCase().indexOf("?action") === -1))
 	{
-		console.log("extension.js onload listener");
-		getTopicClassRows();
-	});
+		window.addEventListener('load', function()
+		{
+			console.log("extension.js load browse page listener");
+			getTopicClassRows();
+		});
+	}
+	
+	if (window.location.href.toLowerCase().indexOf("?action=read") > -1)
+	{
+		window.addEventListener('load', function()
+		{
+			console.log("extension.js load read page listener");
+			getYoutubeTags();
+		});
+	}
 }
 
+function getYoutubeTags()
+{
+	var ytTag = "[yt]";
+	var endTag = "[/yt]";
+	// If the board classes made some FUCKING SENSE this search
+	// would be 100x simpler. But hey, take what you can get.
+	var replies = document.querySelectorAll("td.window");
+	console.log(replies.length);
+	for (var i = 0; i < replies.length; i++)
+	{
+		var outerHTML = replies[i].outerHTML;
+		var startTagIndex = 0;
+		startTagIndex = outerHTML.indexOf(ytTag);
+		var newHTML = outerHTML; // God it's so gross
+		while (startTagIndex > -1)
+		{
+			var endTagIndex = outerHTML.indexOf(endTag, startTagIndex + ytTag.length);
+			if (endTagIndex != -1)
+			{
+				// Grab video ID
+				var fullYt = outerHTML.substring(startTagIndex, endTagIndex + endTag.length);
+				// Send just the inner portion of the tags
+				var ytId = getYoutubeVideoId(fullYt.substring(ytTag.length, fullYt.length - endTag.length));
+				// Replace YT tag contents with YT iframe code
+				var ytIframe = '<iframe width="560" height="315" src="https://www.youtube.com/embed/';
+				ytIframe += ytId + '" frameborder="0" allowfullscreen></iframe>';
+				// I really wish I could replace in-place but Javascript strings are immutable. FML.
+				newHTML = newHTML.replace(fullYt, ytIframe);
+				startTagIndex = endTagIndex + endTag.length;
+			}
+			else
+			{
+				break;
+			}
+			startTagIndex = outerHTML.indexOf(ytTag, startTagIndex + 1);
+		}
+		replies[i].outerHTML = newHTML; // Eugh
+	}
+}
+
+function getYoutubeVideoId(ytStr)
+{
+	var slashIndex = ytStr.indexOf("/");
+	var lastSlashIndex = -1;
+	while (slashIndex > -1)
+	{
+		lastSlashIndex = slashIndex;
+		slashIndex = ytStr.indexOf("/", slashIndex + 1);
+	}
+	if (lastSlashIndex === -1)
+	{
+		// If there isn't a slash at the end of the body, assume it's all an ID
+		return ytStr;
+	}
+	else
+	{
+		var watchStr = "watch?v=";
+		var watchIndex = ytStr.indexOf(watchStr);
+		// This is likely a short-form URL (i.e. https://youtu.be/<ID>)
+		if (watchIndex === -1)
+		{
+			return ytStr.substring(lastSlashIndex + 1);
+		}
+		else
+		{
+			// Long-form URL (i.e. https://www.youtube.com/watch?v=<ID>&<morestuff>
+			var a = watchIndex + watchStr.length;
+			return ytStr.substring(a, ytStr.indexOf("&", a + 1));
+		}
+	}
+}
+// This would have been used for fixing the window.status bullshit, but alas
+// and alack, the old mouseover events can't be removed because they're
+// anonymous functions. HOW FUCKING GREAT!
 function getTopicClassRows()
 {
 	var targetString = "window.status";
